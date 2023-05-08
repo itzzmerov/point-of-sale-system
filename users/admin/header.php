ffe32067ea593320d817e3a4f03546faff884d31
John Rovie R. Balingbing <rvc.blngbng@gmail.com>
2023-05-08 13:06:16 +0800

    Update header.php

--- users/admin/header.php
+++ users/admin/header.php
@@ -36,8 +36,6 @@
 		
 		<!-- LINKS -->
 		<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous"><!-- Bootstrap CSS -->
-		<link rel="stylesheet" href="../../assets/css/dashboard.css"><!-- EXTERNAL CSS -->
-		<link rel="stylesheet" href="../../assets/css/reports.css"><!-- EXTERNAL CSS -->
         <link rel="icon" type="image/x-icon" href="../../assets/images/favicon.ico"><!-- FAVICON -->
 		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"><!-- Font Awesome -->
 		<link rel="preconnect" href="https://fonts.googleapis.com"><!-- GOOGLE FONTS -->
